3ed6d4b5f9d4c357c540b6cb24ed7c0cc62d3dc9
njcyx <njcyx6649250@msn.com>
2023-10-05 11:59:12 -0400

    Update fedexsmartpost.php

--- includes/languages/english/modules/shipping/fedexsmartpost.php
+++ includes/languages/english/modules/shipping/fedexsmartpost.php
@@ -1,5 +1,5 @@
 <?php
 define('MODULE_SHIPPING_FEDEX_SMARTPOST_TEXT_TITLE', 'FedEx');
 define('MODULE_SHIPPING_FEDEX_SMARTPOST_TEXT_METHOD', 'FedEx Ground Economy');
-define('MODULE_SHIPPING_FEDEX_SMARTPOST_TEXT_DESCRIPTION', 'Federal Express<br><br>You will need to have registered an account with FedEx and proper approval from FedEx identity to use this module. Please see the README.TXT file for other requirements.');
+define('MODULE_SHIPPING_FEDEX_SMARTPOST_TEXT_DESCRIPTION', 'Federal Ground Economy / SmartPost<br><br>You will need to have registered an account with FedEx and proper approval from FedEx identity to use this module. Please see the README.TXT file for other requirements.');
 // eof
